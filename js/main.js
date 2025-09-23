6c22f8d3b31e7deee3226cfee537bfc3f2decd29
Enrico Perkins, Jr. <evperkinsjr@gmail.com>
2025-09-23 13:31:12 -0400

    feat: add function to convert spreadsheet into readable format.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ const refs = {
     file: document.getElementById('excel-file-input'),
     convertBtn: document.getElementById('convert-btn'),
 }
+
+async function handleConvert() {
+    const wb = await readWorkbook(refs.file.files[0]);
+    const sheetName = wb.SheetNames[0];
+    const sheet = wb.Sheets[sheetName];
+
+    console.log(sheet);
+}
+
+bindUI({
+    onConvert: handleConvert,
+    refs
+});
